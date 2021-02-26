c92506acf9e3975518844e66f1ed938a03204bac
cmayornavarro <cmayornavarro@gmail.com>
2021-02-26 11:43:09 +0100

    new Spring project

---
+++ camel-microservices/src/main/java/com/camel/microservices/camelmicroservices/CamelMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.camel.microservices.camelmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroservicesApplication.class, args);
+	}
+
+}
